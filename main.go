87b0c0b449fb968f687886190f43b4ddfac0fda1
wangzhuodan <sjx7821211@163.com>
2017-06-27 16:51:17 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,11 +3,4 @@ package main
 import (
 	"github.com/astaxie/beego"
 	"weixin/controllers"
-)
-
-func main() {
-	beego.Info("come in")
-	beego.RegisterController("/", &controllers.MainController{})
-	beego.RegisterController("/weixin", &controllers.MainController{})
-	beego.Run()
-}
+）
